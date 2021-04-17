2cce57f09c2e95d1cf77bee77b540dd32ed2391f
Irarupa <62602926+Irarupa@users.noreply.github.com>
2021-04-17 14:37:43 +0530

    Add files via upload

---
+++ 96 pro/countTheword.py
@@ -0,0 +1,9 @@
+introString = input("enter your introduction ")
+characterCount =0
+wordCount=1
+for i in introString:
+    characterCount = characterCount+1
+print("Number of words into the string ")
+print(wordCount)
+print("Number of character into the string ")
+print(characterCount)    
---
+++ C 96 project/pocketMoney.py
@@ -0,0 +1,8 @@
+pocketMoney = int(input("Enter  the pocket money that you get every month : "))
+if(pocketMoney>500):
+    print("you are a rich a kid ")
+
+elif(pocketMoney>300):
+    print("I know what you feel")
+
+else: print("I know what you feel")    
